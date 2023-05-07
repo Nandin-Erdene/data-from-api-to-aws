7ba55b32a7c2abe0130c6c7db96766916912d3eb
nandin <nandinerdenebs@gmail.com>
2023-05-07 14:34:16 +1000

    added store_api_data function

--- handler.py
+++ handler.py
@@ -1,24 +1,36 @@
 import json
+import requests
+import boto3
+from datetime import datetime
+import pytz
+import os
 
+s3 = boto3.client('s3')
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def store_api_data(event, context):
+    # Latitude and longitude of Melbourne
+    lat = -37.8136
+    lon = 144.9631
+    # openweathermap api key
+    api_key = "58a0c66e12b3ac0d90c63db04f5712e0"
+    # imperial: Fahrenheit, metric: Celsius, default: Kelvin 
+    units = "metric"
+    url = "https://api.openweathermap.org/data/2.5/weather"
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    # Append attributes to the URL
+    url_with_attrs = url + "?lat=" + str(lat) + "&lon=" + str(lon) + "&appid=" + api_key + "&units=" + units
 
-    return response
+    # Fetch the data from the URL
+    response = requests.get(url_with_attrs)
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
+    # Store data in S3
+    timezone = os.environ['TIMEZONE']
+    tz = pytz.timezone(timezone)
+    current_time = datetime.now(tz)
+    s3.put_object(Bucket='melbourne-weather-bucket', Key=current_time.strftime("%Y%m%d/%H:%M:%S") + '.json', Body=json.dumps(response.json()))
+
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+        'statusCode': 200,
+        'body': 'Data stored in S3'
     }
-    """
+    
